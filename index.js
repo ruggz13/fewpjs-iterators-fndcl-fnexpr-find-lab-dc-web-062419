cb364a6e4fa168f9ca48c76c7dd1befb2fd01d99
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-05 17:27:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  if (record.find(function(w) {return w.result === "W"})){
+    return record.find(function(w) {return w.result === "W"}).year
+  }
+  else {
+    return undefined
+  }
+};
+
